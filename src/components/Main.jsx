d5f34d3317c86b632325ffe4972d4674bd2c17e6
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-03-24 18:53:02 +0100

    styling of list changed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Sidebar from './Sidebar'
 
 import News from'./News'
+import NewsLines from './NewsLines'
 const Main = ({articles, setCountry, tiles}) => {
   return (
     <main className='w-full flex flex-row '>
       <Sidebar setCountry={setCountry}/>
-      <News articles={articles} tiles={tiles}/>
+      {tiles?<News articles={articles}/>:
+      <NewsLines articles={articles}/>}
     </main>
   )
 }
---
+++ src/components/NewsLines.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import news from "../../public/assets/news.jpeg";
+
+const NewsLines = ({ articles }) => {
+  return (
+    <div className="flex flex-col h-full justify-center w-full">
+      {articles.map((article, index) => (
+        <div className="p-1 " key={index}>
+          <div className="p-1  md:px-1 flex md:flex-row flex-col justify-between items-center text-black border-2  rounded  w-full">
+            <img
+              src={article.urlToImage ? article.urlToImage : news}
+              alt={article.author}
+              className="w-auto rounded-lg md:max-h-24 md:min-w-[200px] md:min-h-[110px] min-w-full"
+            />
+            <h1 className="text-lg w-auto md:max-w-2xl">{article.title}</h1>
+            <div>
+              <p className="text-gray-600">
+                {article.source.name ? article.source.name : <p>Jane Doe</p>}
+              </p>
+              <p className="text-gray-400">
+                {new Date(article.publishedAt).toLocaleString()}
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NewsLines;
