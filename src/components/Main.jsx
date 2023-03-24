570dac919a830a340f5732239d02dd0e41276513
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-03-24 10:43:33 +0100

    fixed fetching

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,18 @@
 import React, {useState, useEffect} from 'react'
 import Sidebar from './Sidebar'
-
+import axios from 'axios'
+import News from'./News'
 const Main = () => {
   const [articles, setArticles] = useState([])
   const [country, setCountry] = useState('us')
   useEffect(() => {
-    fetch(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=12113e94f90644979de437693a1c9b48`)
-    .then(res => res.json())
-    .then(data=> setArticles(data.articles))
+    const fetchArticles = async () => {
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=12113e94f90644979de437693a1c9b48`)
+      setArticles(response.data.articles)
+      console.log(response)
+    }
+    fetchArticles()
+    //korzystam z axios poniewaz jest szybszy i duzo lepszy od klasycznego fetcha
   }, [country])
   
 
